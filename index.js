2c65036772bec9f57edb5c43e7bc2a2579d88ca1
cruizg_v <cruiz@visanet.com.pe>
2020-08-26 17:48:47 -0500

    c

--- index.js
+++ index.js
@@ -3,31 +3,33 @@ const cISO8583 = require('ciso8583');
 var net = require("net")
 let iso8583Parser = new cISO8583();
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8000
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
-
-var server = net.createServer(function (connection) {
-  console.log('client connected');
-  connection.on('end', function () {
-    console.log('client disconnected');
-  });
-  connection.on('data', function (data) {
-    console.log('recepcionando data');
-    let unpacked = iso8583Parser.unpack(data.toString());
-    var proccessingCode = unpacked.dataElements["3"]
-    console.log('proccessingCode: ' + proccessingCode)
-    if (proccessingCode === null) {
-      connection.write('No se encuentra processing code en la trama');
-    } else {
-      if (process.env[proccessingCode]===undefined) {
-        connection.write('No hay se configuro trama');
-      } else {
-        connection.write(process.env[proccessingCode] || 'No hay Data');
-      }
-    }
-  });
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
+var options = {
+    localAddress: '0.0.0.0'
+}
+var server = net.createServer(options,function (connection) {
+    console.log('client connected');
+    connection.on('end', function () {
+        console.log('client disconnected');
+    });
+    connection.on('data', function (data) {
+        console.log('recepcionando data');
+        let unpacked = iso8583Parser.unpack(data.toString());
+        var proccessingCode = unpacked.dataElements["3"]
+        console.log('proccessingCode: ' + proccessingCode)
+        if (proccessingCode === null) {
+            connection.write('No se encuentra processing code en la trama');
+        } else {
+            if (process.env[proccessingCode] === undefined) {
+                connection.write('No hay se configuro trama');
+            } else {
+                connection.write(process.env[proccessingCode] || 'No hay Data');
+            }
+        }
+    });
 
 });
 
-server.listen(server_port,server_ip_address, function () {
-    console.log( "Listening on " + server_ip_address + ", port " + server_port )
+server.listen(server_port, server_ip_address, function () {
+    console.log("Listening on " + server_ip_address + ", port " + server_port)
 });
